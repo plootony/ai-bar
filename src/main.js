b477ed03848e491413c7c002ba36d7266943e7cd
Антон <a.klimenkov@sitesoft.ru>
2026-07-02 11:17:36 +0500

    first step

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import './style.scss'
+
+document.querySelector('#app').innerHTML = `
+
+ <div class="scene"> 
+   <div class="scene__person scene--person-1 lu"></div>
+  <div class="bubble bubble--bottom-left bubble-1">
+  Не грусти, а то сиськи не будут расти!
+</div>
+      <div class="scene__person scene--person-2 kenzi"></div>
+      <div class="bubble bubble--top-left bubble--thinking bubble-2">
+  Хмм...
+</div>
+
+ <div class="scene__person scene--person-3 mona"></div>
+  <div class="bubble bubble--top-left bubble--thinking bubble-3"> А долго еще ждать текуилу?</div>
+ </div>
+`
