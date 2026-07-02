adbd7f8f740a7fd5b836018ef57e11222bfa4ca6
Антон Клименков <a.klimenkov@sitesoft.ru>
2026-07-02 19:52:25 +0500

    first ai steps

--- src/main.js
+++ src/main.js
@@ -1,18 +1,46 @@
 import './style.scss'
+import { Mistral } from '@mistralai/mistralai';
 
-document.querySelector('#app').innerHTML = `
 
- <div class="scene"> 
-   <div class="scene__person scene--person-1 lu"></div>
-  <div class="bubble bubble--bottom-left bubble-1">
-  Не грусти, а то сиськи не будут расти!
-</div>
-      <div class="scene__person scene--person-2 kenzi"></div>
-      <div class="bubble bubble--top-left bubble--thinking bubble-2">
-  Хмм...
-</div>
 
- <div class="scene__person scene--person-3 mona"></div>
-  <div class="bubble bubble--top-left bubble--thinking bubble-3"> А долго еще ждать текуилу?</div>
- </div>
-`
+// Первый персонаж
+const slot1 = document.querySelector('.slot--1');
+const slot1Bubble = document.querySelector('.slot--1 .bubble');
+const slot1Person = document.querySelector('.slot--1 .person');
+const slot1PersonEmotion = document.querySelector('.slot--1 .person .emotion');
+
+const barmenMessage = document.querySelector('.bubble--barmen textarea');
+
+const client = new Mistral({
+    apiKey: import.meta.env.VITE_MISTRAL_API_KEY,
+});
+
+let messages = [
+    { "role": "user", "content": "Hello!" }
+];
+
+async function main() {
+    const response = await client.beta.conversations.start({
+        agentId: 'ag_019f22921f0c75f7b561ab8f4bdeb5d8',
+        agentVersion: 9,
+        inputs: messages,
+    });
+    slot1Bubble.textContent = JSON.parse(response.outputs[0].content).text
+    slot1PersonEmotion.innerHTML = JSON.parse(response.outputs[0].content).emotion
+
+    console.log(slot1Bubble.textContent);
+
+}
+
+
+
+
+barmenMessage.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter' && e.shiftKey) {
+        messages = [
+            { "role": "user", "content": `${barmenMessage.value}` }
+        ]
+
+        main();
+    }
+});
